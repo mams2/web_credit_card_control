fc7cde2b7bf0e6dc9af31d7ef797c3fcfc4b8be2
Marcilio <mams2@cin.ufpe.br>
2017-01-21 01:12:50 -0300

    create account table

---
+++ db/migrate/20170121024243_create_accounts.rb
@@ -0,0 +1,12 @@
+class CreateAccounts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :accounts do |t|
+      t.string :login, null: false
+      t.string :name, default: ''
+      t.boolean :admin, default: false
+
+      t.index :login, unique: true
+      t.timestamps
+    end
+  end
+end
