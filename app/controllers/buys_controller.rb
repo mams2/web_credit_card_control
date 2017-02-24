be8eb78a386691d14745da875795c195efc41362
Marcilio <mams2@cin.ufpe.br>
2017-02-24 01:13:36 -0300

    finalize buys controller

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -7,7 +7,7 @@ class BuysController < ApplicationController
     if log_in?
       list_of_buyers = {}
       params[:list_of_buyers].each{|buyer| list_of_buyers[buyer[:name]] = buyer[:value].to_f}
-      @buy = Buy.new(params_buys)
+      @buy = Buy.new(buys_params)
       @buy.list_of_buyers = list_of_buyers
       @buy.credit_card_id = params[:credit_card_id]
       if @buy.save
@@ -26,15 +26,26 @@ class BuysController < ApplicationController
   end
 
   def update
-
+    @buy = Buy.find_by(id: params[:id])
+    list_of_buyers = {}
+    params[:list_of_buyers].each{|buyer| list_of_buyers[buyer[:name]] = buyer[:value].to_f}
+    buy = buys_params.to_h
+    buy[:list_of_buyers] = list_of_buyers
+    if @buy.update_attributes(buy)
+      redirect_to CreditCard.find_by(id: params[:credit_card_id])
+    else
+      render 'edit'
+    end
   end
 
   def destroy
-
+    @buy = Buy.find_by(id: params[:id])
+    @buy.destroy if @buy
+    redirect_to CreditCard.find_by(id: params[:credit_card_id])
   end
 
   private
-    def params_buys
+    def buys_params
       params.require(:buy).permit(:purchase_date, :description, :value, :current_payment,
                                   :total_payment)
     end
