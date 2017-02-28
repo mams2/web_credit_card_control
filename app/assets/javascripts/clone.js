54725dc023033fd5a9f7537fb723d19e638f8753
Marcilio <mams2@cin.ufpe.br>
2017-02-28 18:11:58 -0300

    refactoring of forms

--- app/assets/javascripts/clone.js
+++ app/assets/javascripts/clone.js
@@ -1,19 +1,13 @@
-/*
-This script is identical to the above JavaScript function.
-*/
-
 var ct = 1;
 function new_link()
 {
 	ct++;
 	var div1 = document.createElement('div');
 	div1.id = ct;
-	// link to delete extended form elements
-	// var delLink = '<a href="javascript:delIt('+ ct +')">Del</a>';
-	div1.innerHTML = document.getElementById('clone_new_link_tpl').innerHTML; // + delLink;
+	div1.innerHTML = document.getElementById('clone_new_link_tpl').innerHTML;
 	document.getElementById('clone_new_link').appendChild(div1);
 }
-// function to delete the newly added set of elements
+
 function del(eleId)
 {
 	if(ct>1){
@@ -24,7 +18,6 @@ function del(eleId)
 		ct--;
 	}
 	else{
-		alert("É necessário ter pelo menos 1 comprador")
-	}
-	
+		alert("É necessário ter pelo menos 1 comprador");
+	}	
 }
